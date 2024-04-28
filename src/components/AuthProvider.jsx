06ab3801083866f0631b35bdc1f088d46ba92927
Anamul029 <anamhaque.5101@gmail.com>
2024-04-28 19:02:00 +0800

    adding google login

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,23 +1,30 @@
 import { createContext, useState } from "react";
 import auth from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
 
 export const AuthContext = createContext(null)
+// google provider
+const googleProvider = new GoogleAuthProvider()
+
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loader,setLoader]=useState(true)
 
   const createUser=(email,password)=>{
-    return createUserWithEmailAndPassword(auth,email,password)
+    return createUserWithEmailAndPassword(auth,email,password);
   }
 //   userLogin
   const userLogin=(email,password)=>{
-    return signInWithEmailAndPassword(email,password)
+    return signInWithEmailAndPassword(auth,email,password);
+  }
+//   google login
+  const googleLogin=()=>{
+    return signInWithPopup(auth,googleProvider)
   }
 
-
     
-    const AuthInfo = { user,loader,createUser,userLogin }
+    const AuthInfo = { user,loader,createUser,userLogin,googleLogin }
     return (
         <AuthContext.Provider value={AuthInfo}>
             {children}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,81 @@
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
-
+import Swal from "sweetalert2";
 
 
 const Login = () => {
-     const {userLogin}=useContext(AuthContext)
-     console.log(userLogin)
-    const handleLoginn=e=>{
+    const { userLogin, googleLogin } = useContext(AuthContext)
+    const handleLogin = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        // console.log(email, password)
-        // user Login part
+        console.log(email, password)
+        // login
         userLogin(email, password)
-            .then(result => {
-                console.log(result.user);
-                // sweet alert
-                // Swal.fire({
-                //     position: "top-end",
-                //     icon: "success",
-                //     title: "Successfully Register Complete",
-                //     showConfirmButton: false,
-                //     timer: 1500
-                // });
-                // Navigate('/')
-                
+            .then(res => {
+                console.log(res.user)
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Successfully Login",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
             })
             .catch(error => {
-                console.error(error.massege)
-                alert('please provide currect information')
+                console.log(error.message)
             })
+
+    }
+    // google login
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(res => {
+                console.log(res.user)
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Successfully Login",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
+    const handleGitHubLogin = () => {
 
     }
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form onSubmit={handleLoginn} className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+            <div className="">
+                <div className="card shrink-0 w-full md:w-96 max-w-sm  shadow-2xl  bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    {/* another login part */}
+                    <div className="flex flex-col gap-2 mx-auto">
+                        <button onClick={handleGoogleLogin} className="btn bg-yellow-400 w-auto mx-auto">Google Login</button>
+                        <button onClick={handleGitHubLogin} className="btn bg-red-400 w-auto mx-auto mb-3">Github Login Login</button>
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
-                    </div>
-                    <p className="text-center">New to out wensite?please <NavLink to="/register"><span className="font-bold text-blue-800">Register</span></NavLink></p>
-                </form>
-                {/* login container */}
-                <div className="flex flex-col gap-2 mx-auto">
-                    {/* <button onClick={handleGoogleLogin} className="btn bg-yellow-400 w-auto mx-auto">Google Login</button>
-                    <button onClick={handleGitHubLogin} className="btn bg-red-400 w-auto mx-auto mb-3">Github Login Login</button> */}
                 </div>
             </div>
         </div>
