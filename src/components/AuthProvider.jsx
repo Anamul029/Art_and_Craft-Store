e4dc4b618e9cbe38c218f33c0a04722e365786c3
Anamul029 <anamhaque.5101@gmail.com>
2024-04-28 19:45:57 +0800

    editing navbar section

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 
 export const AuthContext = createContext(null)
@@ -12,19 +12,40 @@ const AuthProvider = ({ children }) => {
     const [loader,setLoader]=useState(true)
 
   const createUser=(email,password)=>{
+    setLoader(true)
     return createUserWithEmailAndPassword(auth,email,password);
   }
 //   userLogin
   const userLogin=(email,password)=>{
+    setLoader(true)
     return signInWithEmailAndPassword(auth,email,password);
   }
 //   google login
   const googleLogin=()=>{
+    setLoader(true)
     return signInWithPopup(auth,googleProvider)
   }
 
+//   logout system
+ const logOut=()=>{
+    setLoader(true)
+    return signOut(auth);
+ }
+
+ useEffect(()=>{
+    const unSubscribe=onAuthStateChanged(auth,currentUser=>{
+        console.log('current user',currentUser)
+        setUser(currentUser);
+        setLoader(false)
+
+    })
+    return ()=>{
+        unSubscribe();
+    }
+ },[])
+
     
-    const AuthInfo = { user,loader,createUser,userLogin,googleLogin }
+    const AuthInfo = { user,loader,createUser,userLogin,googleLogin,logOut }
     return (
         <AuthContext.Provider value={AuthInfo}>
             {children}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
 
 
 const Navbar = () => {
-
+    const { user } = useContext(AuthContext)
     const navlinks = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/register">Register</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
         <li><NavLink to="/allArtandCraft">All Art & craft Items</NavLink></li>
-        </>
+    </>
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -17,7 +19,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                       {navlinks}
+                        {navlinks}
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">Art & Craft Store</a>
@@ -28,7 +30,24 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                {
+                    user ? <>
+                        <div className="flex gap-2 justify-center items-center">
+                            <h5>{user.displayName}</h5>
+                            <div className="avatar">
+                                <div className="w-12 rounded-full">
+                                    <img src={user.photoURL}/>
+                                </div>
+                            </div>
+                            <a className="btn">LogOut</a>
+                        </div>
+
+                    </> :
+                        <>
+                            <a className="btn">LogOut</a>
+                        </>
+                }
+
             </div>
         </div>
     );
