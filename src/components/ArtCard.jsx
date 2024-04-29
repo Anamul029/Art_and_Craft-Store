4e7db8f3fedd902d23acf0dde8ad1b7f363c8288
Anamul029 <anamhaque.5101@gmail.com>
2024-04-29 16:01:22 +0800

    adding extra card style

--- src/components/ArtCard.jsx
+++ src/components/ArtCard.jsx
@@ -4,21 +4,7 @@ import Swal from "sweetalert2";
 
 const ArtCard = ({ info }) => {
     const { image, _id, stockStatus, User_Email, User_Name, customization, description, price, processing_time, rating, subcategory } = info;
-    console.log(info)
-
-    // update data
-    // const handleUpdate = (_id) => {
-    //     console.log(_id)
-    //     fetch(`http://localhost:5000/craftItems/${_id}`, {
-    //         method: 'GET',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify()
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => console.log(data))
-    // }
+    // console.log(info)
 
     // delete data
     const handleDelete = (_id) => {
@@ -47,8 +33,7 @@ const ArtCard = ({ info }) => {
                                 text: "Your file has been deleted.",
                                 icon: "success"
                             });
-                            // const remaining=information.filter(infor=>infor._id!==_id)
-                            // setUpdate(remaining)
+                            
                         }
                     })
             }
@@ -56,7 +41,7 @@ const ArtCard = ({ info }) => {
     }
     return (
 
-        <div className="card card-compact md:h-[500px] w-auto bg-yellow-200 shadow-xl">
+        <div className="card card-compact md:h-[600px] w-auto bg-yellow-200 shadow-xl">
             <figure><img className="md:h-[350px] p-4 rounded-xl md:w-[400px]" src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <div className="items-center mx-auto">
@@ -72,9 +57,13 @@ const ArtCard = ({ info }) => {
                     <h3 className="font-semibold">Process time:{processing_time}</h3>
                     <h3 className="font-semibold">Customization:{customization}</h3>
                 </div>
-                <div className="flex justify-around my-3">
-                    <h3 className="font-semibold">Name:{User_Name}</h3>
+                <div className="flex flex-col md:flex-col lg:flex-row items-center justify-around my-3">
+                    <h3 className="font-semibold my-3">Name:{User_Name}</h3>
                     <h3 className="font-semibold">Email:{User_Email}</h3>
+                </div>
+                <div className="flex justify-around">
+                    <h3 className="font-semibold text-blue-900">Rating:{rating}</h3>
+                    
                 </div>
                 <div className="card-actions justify-around">
                     {/* <button className="btn w-3/4 btn-primary"><Link to={`/details/${_id}`}>View Details</Link></button> */}
--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,10 +1,53 @@
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const Update = () => {
+    const {_id}=useParams()
+    console.log(_id)
+
+    const handleUpdateData=e=>{
+        e.preventDefault();
+        const image=e.target.image.value;
+        const item_name=e.target.name.value;
+        const subcategory=e.target.subcategory.value;
+        const description=e.target.description.value;
+        const price=e.target.price.value;
+        const rating=e.target.rating.value;
+        const customization=e.target.customization.value;
+        const processing_time=e.target.processing_time.value;
+        const stockStatus=e.target.stockStatus.value;
+        const information={image,item_name,subcategory,description,price,rating,customization,processing_time,stockStatus}
+        // console.log(information);
+        // update data
+    
+        
+        fetch(`http://localhost:5000/craftItems/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(information)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.modifiedCount>0){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Successfully Data update",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    
+    }
     return (
         <div>
             <h4 className="text-2xl font-semibold text-center my-6">Update Data Here</h4>
-            <form className="bg-blue-50 p-6 rounded-xl">
+            <form onSubmit={handleUpdateData} className="bg-blue-50 p-6 rounded-xl">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     {/* input one */}
                     <div className="form-control">
