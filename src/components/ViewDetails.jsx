90b64726d81c7e8ed959d51c6526a3eb2f30242a
Anamul029 <anamhaque.5101@gmail.com>
2024-04-29 11:01:50 +0800

    adding description and painting paragraph

--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -2,13 +2,13 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 
 const ViewDetails = () => {
-    const information=useLoaderData();
-    const {_id}=useParams();
+    const information = useLoaderData();
+    const { _id } = useParams();
     console.log(_id);
     // console.log(info)
     const info = information.find(info => info._id == _id)
     // console.log(info)
-    const { image,stockStatus, User_Email, User_Name, customization, description, price, processing_time, rating, subcategory } = info;
+    const { image, stockStatus, User_Email, User_Name, customization, description, price, processing_time, rating, subcategory } = info;
 
 
     return (
@@ -17,8 +17,8 @@ const ViewDetails = () => {
             <div className="card-body">
                 <h2 className="card-title">Subategory: {subcategory}</h2>
                 <hr />
-                <p>{description}</p>
-                <p>Residential areas are characterized by diverse dwellings, ranging from single-family homes to high-rise apartments, fostering community bonds and providing essential amenities like schools, parks, and shopping centers for inhabitants' convenience and comfort.</p>
+                <p>Description:{description}</p>
+                <p>Painting and drawing are expressive arts that capture imagination, emotion, and beauty. With brushes or pencils, artists create vibrant worlds, evoking feelings and narratives through colors, lines, shapes, and textures.</p>
                 <div className="flex justify-between my-3">
                     <h3 className="font-semibold">Price:<span>{price}</span></h3>
                     <h3 className="font-semibold">Status: <span>{stockStatus}</span></h3>
@@ -34,8 +34,8 @@ const ViewDetails = () => {
                 </div>
                 <h3 className="my-2 text-xl text-center">Rating:{rating}</h3>
 
-               
-              
+
+
             </div>
         </div>
     );
